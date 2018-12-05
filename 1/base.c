76d86cfb5bdb56ef9d89eb9798c854b2cc07e42d
Jack Lu <jlu9@stuy.edu>
2018-12-05 15:32:57 -0500

    1

--- 1/base.c
+++ 1/base.c
@@ -8,11 +8,21 @@ int main(){
     FILE * f = fopen("./f.txt", "r");
     fseek(f, 0, SEEK_END);
     int size = ftell(f);
+    rewind(f);
+    char * array = malloc(size);
     //rewind(f);
-    printf("%ld\n", ftell(f));
+    //printf("%ld\n", ftell(f));
     
+    char * line;
+    size_t n = 0;
+    ssize_t read;
+    int num = 0;
+    while( (read = getline(&line, &n, f)) != -1){
+        num += atoi(line);
+    }
 
-    
+    printf("%d\n", num);
+
 
     fclose(f);
     return 0;
