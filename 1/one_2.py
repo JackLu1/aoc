43ffc994e654b208c37176b79d9904826f1ff9b0
jlu9 <jlu9@stuy.edu>
2018-12-05 17:36:29 -0500

    no 1

--- 1/one_2.py
+++ 1/one_2.py
@@ -1,4 +1,22 @@
+sum = 0
+a = []
 with open("f.txt") as f:
-    a = f.read()
+  line = 1
+  while line:
+    line = f.readline().rstrip("\n")
+    if line != '':
+      sum += int(line)
+      if sum in a:
+        print(sum)
+        break
+      else:
+        a.append(sum)
+    else:
+      print('aaaa')
+      f.seek(0,0)
+      line = f.readline().rstrip("\n")
+      
+    
+    
 
-print(a)
+f.close()
